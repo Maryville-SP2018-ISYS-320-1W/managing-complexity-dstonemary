5e45e5ff8e7f4be735436fe86b982343fb877874
dstonemary <37548808+dstonemary@users.noreply.github.com>
2018-04-03 00:16:17 +0530

    Week2 BoxWIndow

---
+++ src/BoxWindow.java
@@ -0,0 +1,20 @@
+
+public class BoxWindow {
+
+	
+	public static void main(String[] args) {
+		System.out.print("+===+===+");
+		for(int i = 0; i<2 ; i++){			
+			System.out.println();
+			for(int j = 0 ; j <3 ; j++){				
+				System.out.print("|");
+				System.out.print("   |   ");
+				System.out.print("|");
+				System.out.println();				
+			}
+			System.out.print("+===+===+");
+		}
+		
+	}
+
+}
